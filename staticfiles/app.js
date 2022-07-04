d974864a6b91dcb6f271e84d8a8d76d789efacb7
Taiwo <ajayitaiwotunta@gmail.com>
2022-07-05 00:36:22 +0100

    initial commit

--- staticfiles/app.js
+++ staticfiles/app.js
@@ -88,57 +88,42 @@ function GetData(entries) {
           console.log(post.has_next);
 
           const html = `
-                  <div class="post-card">
-                    <div class="d-flex">
-                      <a href="/user_profile/${post.profile_id}">
-                    <img src="${
-                      post.profile_image
-                    }" alt="user-image" class="user-image">
-                  </a>
-                  <div class="post-heading">
-                    <h4 class="username">${post.username}</h4>
-                    <small class="date">${post.date}</small>
-                  </div>
-                </div>
-                <div class="post-info">
-                  <h3 class="title">${post.title === null ? "" : post.title}</h3
-                  ${
-                    post.post_image_name == "kindpng_4517876.png"
-                      ? ""
-                      : `<img src="${post.post_image}" alt="" class="image">`
-                  }
-                    <p class="body">
-                      ${
-                        post.body.length > 150
-                          ? post.body.slice(0, 150) +
-                            `... <a href="post_details/${post.post_id}" class="read-more cl-fb">Read more</a>`
-                          : post.body
-                      }
-                    </p>
-                      
-                </div>
-                <div class="comments d-flex">
-                  <a href="post_details/${
-                    post.post_id
-                  }"><i class="far fa-comment-alt comment"> ${
-            post.comment
-          }</i></a>
-                  <div class="">
-                    <i class="fa${
-                      post.user_liked ? "s" : "r"
-                    } fa-thumbs-up likes" id="${post.post_id}"></i>
-                    <span class="total_likes">${post.total_comment}</span>
-                    </div>
-                    </div>
-                    </div> 
-                    </div> 
+      <div class="post-card">
+
+          <div class="d-flex">
+            <a href="/user_profile/${post.profile_id}">
+              <img src="${post.profile_image}" alt="user-image" class="user-image" />
+            </a>
+            <div class="post-heading">
+              <h4 class="username">${post.username}</h4>
+              <small class="date">${post.date}</small>
             </div>
-            `;
+          </div>
+        
+          <div class="post-info">
+            <h3 class="title">${post.title === null ? "" : post.title}</h3>
+            ${post.post_image_name == "kindpng_4517876.png"
+              ? ""
+              : `<img src="${post.post_image}" alt="" class="image">`
+            }
+            <p class="body">${post.body.length > 150 ? post.body.slice(0, 150) + `... <a href="post_details/${post.post_id}" class="read-more cl-fb">Read more</a>` : post.body}</p>
+          </div>
+          
+          <div class="comments d-flex">
+            <a href="post_details/${post.post_id}"><i class="far fa-comment-alt comment"> ${post.comment}</i></a>
+            <div class="">
+              <i class="fa${post.user_liked ? " s" : "r"} fa-thumbs-up likes" id="${post.post_id}"></i>
+              <span class="total_likes">${post.total_comment}</span>
+            </div>
+          </div>
+          
+      </div>
+      `;
           laodData.insertAdjacentHTML("beforeBegin", html);
 
           if (!post.has_next) {
-            observer.unobserve(laodData)
-            laodData.style.display = 'none'
+            observer.unobserve(laodData);
+            laodData.style.display = "none";
           }
         });
       });
