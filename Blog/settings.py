d420e3dd39901abe85e6eeb469e0b51886dc70cf
Taiwo <ajayitaiwotunta@gmail.com>
2022-07-09 02:58:29 +0100

    Changed Debug Settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('IS_DEVELOPMENT', True)
+DEBUG = (os.getenv('IS_DEVELOPMENT') == 'True')
 
-ALLOWED_HOSTS = [os.getenv('APP_HOST'), '127.0.0.1','taiwodjangoblog.herokuapp.com']
+ALLOWED_HOSTS = [os.getenv('APP_HOST'), '127.0.0.1']
 
 
 # Application definition
